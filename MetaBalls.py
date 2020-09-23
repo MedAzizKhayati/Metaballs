44e74cae085544a754d46deb8faff7c6cc1739ba
losimix <68187294+losimix@users.noreply.github.com>
2020-09-23 02:13:13 +0100

    Update MetaBalls.py

--- MetaBalls.py
+++ MetaBalls.py
@@ -1,99 +1,113 @@
 import pygame
 import math
-from pygame import gfxdraw
-import random
-import numpy
-import numba as nb
-from _thread import *
+import time
 import os
+from pygame import gfxdraw
+import numpy as np
+import random
+import numba as nb
 
 pygame.init()
+width = 1000
 height = 720
-width = 1280
-win = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Metaballs")
 CORES = os.cpu_count()
-balls = []
-screen = numpy.zeros((width, height, 3), dtype=numpy.int32)
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Gravity")
+click = False
+clicked = False
+blobs = []
 
-class metaball:
+
+class blob:
     def __init__(self):
         self.r = random.randint(30, 50)
-        self.coords = [random.randint(30, 1250), random.randint(30, 690)]
-        self.vel = [random.randint(-3, 3), random.randint(-3, 3)]
+        self.rgb = [1, 0, 0]
+        self.rgb[random.randint(0, 2)] = 1
+        self.coords = [random.randint(50, width - 50), random.randint(50, height - 50)]
+        self.vel = [random.randint(-6, 6), random.randint(-6, 6)]
         while self.vel == [0, 0]:
-            self.vel = [random.randint(-3, 3), random.randint(-3, 3)]
+            self.vel = [random.randint(-6, 6), random.randint(-6, 6)]
 
     def move(self):
-        if self.r >= self.coords[0] or self.coords[0] >= 1280 - self.r:
+        if self.r >= self.coords[0] or self.coords[0] >= width - self.r:
             self.vel[0] *= -1
-        if self.r >= self.coords[1] or self.coords[1] >= 720 - self.r:
+        if self.r >= self.coords[1] or self.coords[1] >= height - self.r:
             self.vel[1] *= -1
         self.coords[0] += self.vel[0]
         self.coords[1] += self.vel[1]
 
-    def draw(self, win):
-        draw_circle(win, self.coords[0], self.coords[1], self.r, (255, 0, 100))
-
-
-def draw_circle(surface, x, y, radius, color):
-    gfxdraw.aacircle(surface, x, y, radius, color)
-    gfxdraw.filled_circle(surface, x, y, radius, color)
-
-
-def ball_generation():
-    global balls
-    num = random.randint(3, 10)
-    for i in range(0, num):
-        balls.append(metaball())
-
 
 @nb.njit(parallel=True, fastmath=True)
-def pixel():
-    global win, screen
+def update_pixels(coords: np.array, radius: np.array, color: np.array, screen: np.array):
     for start in nb.prange(CORES):
         for x in range(start, width, CORES):
             for y in range(height):
-                color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-                for c in range(3):
-                    print(screen[x, y, c])
-                    screen[x, y, c] = 2
+                screen[x, y] = 0
+                for i, j, k in zip(coords, radius, color):
+                    dx = x - i[0]
+                    dy = y - i[1]
+                    dist = dx * dx + dy * dy + 0.01
+                    r = int(j * j / dist * 255)
+                    r = (r * k[0] << 16) + (r * k[1] << 8) + r * k[2]
+                    screen[x, y] += r
+                    if screen[x][y] > 16777215:
+                        screen[x][y] = 16777215
+    return screen
 
 
-def pixel_colors(win):
-    for i in balls:
-        for j in balls:
-            if j != i:
-                a = numpy.array(i.coords)
-                b = numpy.array(j.coords)
-                if numpy.linalg.norm(a - b) < 100:
-                    pass
-            # for k in range(min(a[0], b[0]), max(a[0], b[0])):
-            #     for l in range(min(a[1] - i.r, a[1] + i.r, b[1] - j.r, b[1] + j.r),
-            #                    max(a[1] - i.r, a[1] + i.r, b[1] - j.r, b[1] + j.r)):
-            #         c = numpy.array([k, l])
-            #         if numpy.linalg.norm(a - c) < 100 or numpy.linalg.norm(b - c) < 100:
-            #             gfxdraw.pixel(win, k, l, (255, 0, 100))
+def custom_blob(mx, my):
+    global click, clicked, blobs
+    if click and not clicked:
+        blobs.append(blob())
+        blobs[len(blobs) - 1].coords = [mx, my]
+        blobs[len(blobs) - 1].vel = [0, 0]
+        clicked = True
+    elif click and clicked:
+        blobs[len(blobs) - 1].coords = [mx, my]
+    elif not click and clicked:
+        clicked = False
+        blobs.pop(len(blobs) - 1)
 
 
 def main():
-    global screen
+    global click, clicked, blobs
     run = True
-    ball_generation()
+    mx, my = 0, 0
+    j = 0
     clock = pygame.time.Clock()
+    for i in range(5):
+        blobs.append(blob())
+    screen = np.zeros(shape=(width, height))
     while run:
-        clock.tick(144)
-        #win.fill((255, 0, 255))
-        pixel()
-        pygame.surfarray.blit_array(win, screen)
-        for i in balls:
+        dt = clock.tick() / 1000
+        j += 1
+        win.fill((255, 255, 255))
+        coords = []
+        r = []
+        color = []
+        custom_blob(mx, my)
+        for i in blobs:
             i.move()
-            i.draw(win)
+            coords.append(i.coords)
+            r.append(i.r)
+            color.append(i.rgb)
+        coords = np.array(coords)
+        r = np.array(r)
+        color = np.array(color)
+        update_pixels(coords, r, color, screen)
+        pygame.surfarray.blit_array(win, screen)
+        click = False
+        if pygame.mouse.get_pressed()[0]:
+            click = True
+            mx, my = pygame.mouse.get_pos()
+        if j % 20 == 0:
+            print(int(1 / dt))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
         pygame.display.update()
 
 
-main()
+if __name__ == "__main__":
+    main()
 pygame.quit()
